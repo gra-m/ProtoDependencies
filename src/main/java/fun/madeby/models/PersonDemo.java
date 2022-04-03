d0eaf624f372f1ce1ca68e00566a3fe24f55c6e1
gra-m <91182298+gra-m@users.noreply.github.com>
2022-04-03 19:14:44 +0200

    closes #4 Files.write _ Files.readAllBytes

--- src/main/java/fun/madeby/models/PersonDemo.java
+++ src/main/java/fun/madeby/models/PersonDemo.java
@@ -1,17 +1,34 @@
 package fun.madeby.models;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by Gra_m on 2022 03 30
  */
 
 public class PersonDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        Person sam = Person.newBuilder()
                .setName("sam")
                .setAge(10)
                .build();
 
-        System.out.println(sam);
+        System.out.println("Original\n" + sam);
+
+       // Create serialized person
+       Path path = Paths.get("Person.ser");
+       Files.write(path, sam.toByteArray());
+
+       // Retrieve serialized person
+       byte[] retrievedByteArray = Files.readAllBytes(path);
+       Person newSam = Person.parseFrom(retrievedByteArray);
+
+       System.out.println("Retrieved\n" + newSam);
+
+
 
     }
 }
